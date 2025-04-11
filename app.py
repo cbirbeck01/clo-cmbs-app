5b613a5c6ddec494ca97c6c33f213e6bdd7b6459
cbirbeck01 <charles.birbeck@gmail.com>
2025-04-11 13:23:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,25 @@ import streamlit as st
 from clo_model import run_clo_model
 from cmbs_model import run_cmbs_model
 
-st.set_page_config(page_title="Structured Products Explorer", layout="centered")
+st.set_page_config(page_title="Asset Backed Securities Visualizer", layout="centered")
+
 view = st.query_params.get("view", "home")
-if view == "home":
+
+if view=="home":
     st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-    st.title("📊 Structured Products Explorer")
+    st.title("Asset Backed Securities Visualizer")
     st.markdown("### Select a product type to begin:")
-    col1, col2 = st.columns(2)
+
+    col1, col2=st.columns(2)
+
     with col1:
         if st.button("CLO"):
-            st.experimental_set_query_params(view="clo")
+            st.query_params["view"] = "clo"
+
     with col2:
         if st.button("CMBS"):
-            st.experimental_set_query_params(view="cmbs")
+            st.query_params["view"] = "cmbs"
+
 elif view=="clo":
     run_clo_model()
 

3513ad4d21a506a2afdb71f0efd755b03cb5ba58
cbirbeck01 <charles.birbeck@gmail.com>
2025-04-11 13:09:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import streamlit as st
 from clo_model import run_clo_model
 from cmbs_model import run_cmbs_model
 
-st.set_page_config(page_title="Structured Finance Dashboard", layout="wide")
+st.set_page_config(page_title="Asset Backed Securities Dashboard", layout="centered")
 
-model_choice = st.sidebar.radio("Select Model", ["CLO", "CMBS"])
+st.title("Asset Backed Securities Dashboard")
 
-if model_choice == "CLO":
+st.markdown("### Choose security to begin:")
+
+app_choice: st.selectbox("Select Application:", ["-", "CLO", "CMBS"])
+if app_choice == "CLO":
     run_clo_model()
 
-elif model_choice == "CMBS":
+elif app_choice == "CMBS":
     run_cmbs_model()

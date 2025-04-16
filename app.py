cbbca254a372901f29b38eea87a6d015848164ab
cbirbeck01 <charles.birbeck@gmail.com>
2025-04-16 01:10:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
+
 import streamlit as st
 from clo_model import run_clo_model
 
-
-
-st.set_page_config(page_title="Asset Backed Securities Visualizer", layout="wide")
-
+st.set_page_config(page_title="CLO Waterfall Model", layout="wide")
 
 st.markdown("""
 <style>
 div.stButton > button {
-    background-color:#03893e;
-    color:white;
-    padding:0.75em 2em;
-    font-size:1.1em;
-    font-weight:600;
-    border-radius:10px;
-    border:none;
-    transition:background-color 0.3s ease;
+    background-color: #03893e;
+    color: white;
+    padding: 0.75em 2em;
+    font-size: 1.1em;
+    font-weight: 600;
+    border-radius: 10px;
+    border: none;
+    transition: background-color 0.3s ease;
 }
 div.stButton > button:hover {
-    background-color:#026c32;
+    background-color: #026c32;
 }
 </style>
 """, unsafe_allow_html=True)
-
 
 view = st.query_params.get("view", "home")
 
@@ -31,17 +28,14 @@ if view == "home":
     st.title("Asset Backed Securities Visualizer")
     st.markdown("### Select a product type to begin:")
 
-    col1= st.columns(1)
-
-    with col1:
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
         if st.button("CLO Model"):
             st.query_params["view"] = "clo"
             st.rerun()
 
-
 elif view == "clo":
     run_clo_model()
-
 
 else:
     st.error("Invalid view.")
